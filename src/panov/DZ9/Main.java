4f22aa43e2f4212f2f1db61cd2dc34d9485f1da5
Aleksandr Panov <san9.aleks@gmail.com>
2024-03-18 23:19:26 +0300

    test

---
+++ src/panov/DZ9/Main.java
@@ -0,0 +1,10 @@
+package panov.DZ9;
+
+public class Main {
+    public static void main(String[] args) {
+        Phone iphone = new Phone();
+        Phone nokia = new Phone();
+        Phone samsung = new Phone();
+        System.out.println(iphone);
+    }
+}
